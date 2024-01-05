afa1336839ea22fff751b86117e271cf2a9cf16b
Omar Zakaria <omarzakarya9@gmail.com>
2024-01-04 20:39:05 -0500

    Styling fixes

--- app/components/ZProfileCard/ZProfileCard.tsx
+++ app/components/ZProfileCard/ZProfileCard.tsx
@@ -12,13 +12,13 @@ const ZProfileCard: React.FC<ZProfileCardProps> = ({ title, image, className, ch
     return (<div id="profile-card" className={`grid grid-cols-12 shadow-lg rounded-lg px-4 py-8 dark:bg-gray-700 bg-gray-100 ${className}`}>
         {/* <!-- Photo Section --> */}
         {image &&
-            <div className="col-span-12 md:col-span-4 mb-5 md:mb-0">
-                <Image src={image} alt="Profile Image" className="rounded-full w-full h-auto" />
+            <div className="col-span-12 sm:col-span-6 xl:col-span-4 mb-5 md:mb-0 flex justify-center">
+                <Image src={image} alt="Profile Image" className="rounded-full w-60  h-auto" />
             </div>
         }
 
         {/* <!-- Information Section --> */}
-        <div className={`ml-4 ${image ? 'col-span-12 md:col-span-8 ' : 'col-span-12 md:col-span-12 '}`}>
+        <div className={`ml-4 ${image ? 'col-span-12 sm:col-span-6 xl:col-span-8 ' : 'col-span-12 md:col-span-12 '}`}>
             <h2 className="text-2xl font-semibold">{title}</h2>
             {children}
         </div>
